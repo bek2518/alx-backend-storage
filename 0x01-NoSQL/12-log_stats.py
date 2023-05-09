f1fc45a2a63669e0ee9e6145331c3a3d2cfc9fdc
bek2518 <bereketb20@hotmail.com>
2023-05-09 15:28:36 +0300

    Provides stat about nginx

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,16 +4,16 @@ Python script that provides some stats about Nginx logs stored in MongoDB
 '''
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    with MongoClient() as client:
+        db = client.logs
+        logs = db.nginx.count_documents({})
+        print(str(logs) + ' logs')
+        print('Methods:')
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        for method in methods:
+            count_method = db.nginx.count_documents({"method": method})
+            print('\tmethod ' + method + ': ' + str(count_method))
 
-with MongoClient() as client:
-    db = client.logs
-    logs = db.nginx.count_documents({})
-    print(str(logs) + ' logs')
-    print('Methods:')
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count_method = db.nginx.count_documents({"method": method})
-        print('\tmethod ' + method + ': ' + str(count_method))
-
-    stat_chk = db.nginx.count_documents({"method": "GET", "path": "/status"})
-    print(str(stat_chk) + ' status check')
+        st_chk = db.nginx.count_documents({"method": "GET", "path": "/status"})
+        print(str(st_chk) + ' status check')
