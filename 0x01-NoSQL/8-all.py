0293fba6e0644e87362cf00cc24ba75a123577cd
bek2518 <bereketb20@hotmail.com>
2023-05-09 12:16:00 +0300

    Lists all documents using python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Python function that lists all documents in a collection
+'''
+
+
+def list_all(mongo_collection):
+    '''
+    Function that lists all documents in mongo_collection
+    '''
+    if mongo_collection is not None:
+        return (mongo_collection.find())
+    return []
