1c26e0fc3103715cc30875aba6c798d69fbbd685
bek2518 <bereketb20@hotmail.com>
2023-05-10 17:07:35 +0300

    Store history of input and output

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,31 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwds)
     return wrapper
 
+
+def call_history(method: Callable) -> Callable:
+    '''
+    Method that defines a decorator method that takes single method
+    Callable argument and returns a callable
+    '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''
+        wrapper function that stores the history of inputs and outputs
+        '''
+        key = method.__qualname__
+        input__list_keys = key + ":inputs"
+        output__list_keys = key + ":outputs"
+
+        input_argument = str(args)
+        self._redis.rpush(input__list_keys, input_argument)
+
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output__list_keys, output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     '''
     Cache class that stores an instance of reddit client
@@ -35,6 +60,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         Method that takes a data argument and returns a string
