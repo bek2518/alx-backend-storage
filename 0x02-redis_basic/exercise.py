e4e06b2341483c377e2337568c438d45a7f32454
bek2518 <bereketb20@hotmail.com>
2023-05-10 16:17:13 +0300

    Creates get, get_str and get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Introduces to Redis and caching
 '''
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -25,3 +25,28 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        '''
+        Method that takes argument key and optional callable argument
+        fn which will be used to convert the data back to the desired format
+        '''
+        if fn is None:
+            return (self._redis.get(key))
+
+        return fn(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        '''
+        Method that automatically parametrize Cache.get with string
+        conversion function
+        '''
+        return (str(self.get(key)))
+
+    def get_int(self, key: str) -> int:
+        '''
+        Method that automatically parametrize Cache.get with integer
+        conversion function
+        '''
+        return (int(self.get(key)))
